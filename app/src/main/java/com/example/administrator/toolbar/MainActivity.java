5e927a1fbbd4fb98fd54c60bd37b4618b0ce44f6
xubotao <13051672112@163.com>
2017-05-23 14:06:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/toolbar/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.administrator.toolbar;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 经测试在代码里直接声明透明状态栏更有效
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //if(toolbar!=null) {
+            toolbar.inflateMenu(R.menu.zhihu_toolbar_menu);
+            toolbar.setNavigationIcon(R.drawable.back_pressed);
+        //}
+    }
+}
